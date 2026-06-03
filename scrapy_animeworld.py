7f93a95ce10c53dba9c42a45818bddab231afee1
Kedryn <marcopaglioni@gmail.com>
2026-06-03 16:46:19 +0200

    fix

--- scrapy_animeworld.py
+++ scrapy_animeworld.py
@@ -125,4 +125,193 @@ def load_anime_list(file_path):
                         print(f"Riga ignorata a causa di un numero di colonne non corrispondente: {row}")
             print(f"Caricati {len(data)} anime esistenti dal file '{file_path}'.")
         except Exception as e:
-            print(f"Errore durante il caricamento del file '{file_path}': {
+            print(f"Errore durante il caricamento del file '{file_path}': {e}")
+            return {}
+    return data
+
+# Configura il logging
+log_file = "scrapy_animeworld.log"
+if os.path.exists(log_file):
+    os.remove(log_file)
+logging.basicConfig(
+    filename=log_file,
+    filemode='a',
+    format='%(asctime)s [%(levelname)s] %(message)s',
+     # Imposta il livello di logging a INFO per registrare i messaggi aggiunti
+    level=logging.INFO,
+    force=True
+)
+
+# Funzione di utilità per loggare anche su console
+def log(message, level="info"):
+    print(message)
+    if level == "info":
+        logging.info(message)
+    elif level == "warning":
+        logging.warning(message)
+    elif level == "error":
+        logging.error(message)
+    elif level == "debug":
+        logging.debug(message)
+    else:
+        logging.info(message)
+
+def scrape_animeworld():
+    """
+    Estrae titoli di anime, numeri di episodio e l'URL del primo episodio da animeworld.ac.
+    Salva i dati in un file CSV.
+    """
+    base_url = "https://www.animeworld.ac"
+    csv_file_path = "anime_list.csv"
+    
+    # Aggiunto un blocco per recuperare il numero massimo di pagine dinamicamente
+    # Inizializza il numero massimo di pagine con un valore di fallback
+    max_pages_to_scrape = 500
+    
+    # URL per la pagina di lista principale
+    main_list_url = f"{base_url}/az-list"
+    print(f"Recupero il numero totale di pagine dalla pagina principale: {main_list_url}")
+    main_list_html = get_html_content(main_list_url)
+
+    if main_list_html:
+        # Cerca il valore della paginazione nel tag script
+        match = re.search(r'window\.paginationMaxPage\s*=\s*parseInt\("(\d+)"\);', main_list_html)
+        if match:
+            # Estrae il numero e lo converte in intero
+            max_pages_to_scrape = int(match.group(1))
+            log(f"Trovato il numero totale di pagine: {max_pages_to_scrape}", "info")
+        else:
+            log(f"Valore di paginazione non trovato, uso il valore di fallback: {max_pages_to_scrape}", "warning")
+    else:
+        log(f"Impossibile recuperare la pagina principale, uso il valore di fallback: {max_pages_to_scrape}", "warning")
+
+    log("Inizio dell'estrazione...", "info")
+
+    # Carica i dati esistenti dal file CSV in un array
+    existing_anime_data = load_anime_list(csv_file_path)
+
+
+    page_number = 1
+    while page_number <= max_pages_to_scrape:
+        list_url = f"{base_url}/az-list?page={page_number}"
+        log(f"Processo lista a pagina: {page_number}", "info")
+        list_html = get_html_content(list_url)
+
+        if not list_html:
+            log("Impossibile recuperare la pagina, interruzione dell'estrazione.", "error")
+            break
+                
+        list_soup = BeautifulSoup(list_html, 'html.parser')
+        # Selettore aggiornato per i tag 'a' con classe 'name'
+        anime_items = list_soup.select('div.items a.name')
+
+        log(f"Processo lista a pagina: {page_number}", "info")  
+
+        if not anime_items:
+            log("Nessun anime trovato in questa pagina. Probabile fine della lista.", "info")
+            break
+
+        for item in anime_items:
+            # Aggiornato per ottenere il titolo dall'attributo 'data-jtitle'
+            anime_title = item.get('data-jtitle', '').strip().replace('#', '')
+            
+            # Sanitizza il titolo per usarlo come nome di percorso
+            download_path = sanitize_title(anime_title)
+            
+            # Controlla se l'anime esiste già nel nostro dizionario in memoria
+            is_existing = download_path in existing_anime_data
+
+            if is_existing and not forza:
+                log(f"  Anime '{anime_title}' già esistente, salto...", "info")
+                continue  # Salta al prossimo elemento nel ciclo
+
+            log(f"  Recupero dettagli per: {anime_title}", "info")
+
+            anime_page_url = f"{base_url}{item['href']}"
+            anime_page_html = get_html_content(anime_page_url)
+            if anime_page_html:
+
+                # Usa la funzione per ottenere il numero di episodi (utile per l'ultimo episodio)
+                primo_episodio_nuovo, ultimo_episodio_nuovo = get_episode_numbers(anime_page_html)
+                if primo_episodio_nuovo == '-1' or ultimo_episodio_nuovo == '-1':
+                    log(f"  Episodi non trovati per {anime_title}, salto...", "warning")
+                else:
+                    # Trova il link del primo episodio usando il suo ID specifico
+                    first_episode_link = BeautifulSoup(anime_page_html, 'html.parser').select_one('#alternativeDownloadLink')
+
+                    if first_episode_link:
+                        episode_url_nuovo = first_episode_link['href']
+                        log(f"   [OK] Link alternativo trovato per '{anime_title}': {episode_url_nuovo}", "info")
+                        
+                        # Cerca il numero dell'episodio nel link usando le espressioni regolari per entrambi i casi SUB e ITA
+                        match = re.search(r'_(\d+)_(?:SUB|ITA)', episode_url_nuovo)
+
+                        if match:
+                            episode_num_from_url = match.group(1)
+                            # Se il numero dell'episodio è '01' (o simili), lo sostituisce con '*'
+                            if episode_num_from_url in ['01', '001', '0001', '00']:
+                                episode_url_nuovo = re.sub(r'Ep_\d+_(SUB|ITA)', 'Ep_*_\\1', episode_url_nuovo)
+
+                        # Inizializza ultimoaggiornamento con un valore di default
+                        ultimoaggiornamento = ""
+                        if download_path in existing_anime_data and existing_anime_data[download_path]['ultimoaggiornamento'] == "":
+                            ultimoaggiornamento = '1900-01-01'
+                        elif download_path in existing_anime_data:
+                            ultimoaggiornamento = existing_anime_data[download_path]['ultimoaggiornamento']
+                        else:
+                            ultimoaggiornamento = ""
+
+                        # Dati da aggiungere o aggiornare
+                        data_to_add = {
+                            'url_primo_episodio': episode_url_nuovo,
+                            'ultimo_episodio': ultimo_episodio_nuovo,
+                            'stagione_episodio': '01',
+                            'download_path': download_path,
+                            'titolo': anime_title,
+                            'ultimoaggiornamento':   ultimoaggiornamento
+                        }
+
+                        # Se l'anime esiste e forza = True, aggiorna i dati ma mantiene il primo episodio
+                        if is_existing and forza:
+                            data_to_add['primo_episodio'] = existing_anime_data[download_path]['primo_episodio']
+                            if ultimo_episodio_nuovo > existing_anime_data[download_path]['ultimo_episodio']:
+                                data_to_add['ultimoaggiornamento'] = time.strftime('%Y-%m-%d')
+                            existing_anime_data[download_path] = data_to_add
+                            log(f"  Aggiornato: {anime_title} (mantenuto primo episodio esistente) {ultimoaggiornamento}", "info")
+                        # Altrimenti, è un nuovo anime, quindi lo aggiunge completamente
+                        else:
+                            data_to_add['ultimoaggiornamento'] = time.strftime('%Y-%m-%d')
+                            data_to_add['primo_episodio'] = primo_episodio_nuovo
+                            existing_anime_data[download_path] = data_to_add
+                            log(f"  Aggiunto: {anime_title} episodi {primo_episodio_nuovo} - {ultimo_episodio_nuovo}  {ultimoaggiornamento}", "info")
+
+                    else:
+                        log(f"   [WARN] Link di download alternativo non trovato per '{anime_title}'", "warning")
+
+                    # Aggiungi un piccolo ritardo per evitare di sovraccaricare il server
+                    #time.sleep(1)
+            else:
+                log(f"  Impossibile recuperare la pagina dell'anime: {anime_title}", "error")
+        # Incrementa il numero di pagina per la prossima iterazione 
+        page_number += 1
+
+
+    # Riordina existing_anime_data usando il campo 'ultimoaggiornamento' in ordine discendente e crea un nuovo dizionario ordinato
+    sorted_anime_data = dict(
+        sorted(
+            existing_anime_data.items(),
+            key=lambda x: x[1].get('ultimoaggiornamento', ''),
+            reverse=True
+        )
+    )
+    log(f"\nScrittura finale dei dati in '{csv_file_path}'...", "info")
+    fieldnames = ['url_primo_episodio', 'primo_episodio', 'ultimo_episodio','stagione_episodio', 'download_path','titolo','ultimoaggiornamento']
+    with open(csv_file_path, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames, delimiter='#')
+        for row in sorted_anime_data.values():
+            writer.writerow(row)
+
+    log(f"\nEstrazione e salvataggio completati! Dati salvati in '{csv_file_path}'.", "info")
+
+if __name__ == "__main__":
+    scrape_animeworld()
